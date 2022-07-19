5c009701b0cd8ba26d6fd6d36b032d0305075bdd
Ali Noual <noualdev@gmail.com>
2022-07-19 18:05:35 +0100

    Refactorisation

--- app/Services/File.php
+++ app/Services/File.php
@@ -25,9 +25,8 @@ class File
         return fopen('upload/edi/' . $this->name, 'r');
     }
 
-    public static function read($path)
+    public static function read($file)
     {
-        $file = fopen('upload/edi/' . $path, 'r');
         while (!feof($file)) {
             $line = trim(fgets($file));
             $payment = [];
@@ -56,36 +55,21 @@ class File
     private static function ee(?string $line = null, ?int $payment_id = null): array
     {
         if ($line) {
-            $discount_header = trim(mb_substr($line, 0, 4, 'UTF-8')); // Entête de la remise
-            $iob = trim(mb_substr($line, 4, 3, 'UTF-8')); // Identifiant de la banque du donneur d’ordre
-            $nto = trim(mb_substr($line, 7, 3, 'UTF-8')); // Nature et type d’opération
-            $nb = trim(mb_substr($line, 10, 1, 'UTF-8')); // Nature des fonds
-            $pi = trim(mb_substr($line, 11, 1, 'UTF-8')); // Indicateur de présence RIB/BAN
-            $rib = trim(mb_substr($line, 12, 20, 'UTF-8')); // RIB
-            $ip = trim(mb_substr($line, 32, 4, 'UTF-8')); // Préfix IBAN
-            $name = trim(mb_substr($line, 36, 50, 'UTF-8')); // Non, prénom ou raison sociale du donneur d’ordre
-            $address = trim(mb_substr($line, 86, 70, 'UTF-8')); // Adresse du donneur d’ordre
-            $date = trim(mb_substr($line, 156, 8, 'UTF-8')); // Date de la remise de l’ordre
-            $discount_reference = trim(mb_substr($line, 164, 3, 'UTF-8')); // Référence de la remise
-            $discount_on = trim(mb_substr($line, 167, 6, 'UTF-8')); // Nombre d’opération dans la remise
-            $totalAmount = trim(mb_substr($line, 173, 16, 'UTF-8')); // Montant total
-            $filler = trim(mb_substr($line, 189, 31, 'UTF-8')); // Filler
-            // $data = [
-            //     'Entête de la remise' => $discount_header,
-            //     'Identifiant de la banque du donneur d’ordre' => $iob,
-            //     'Nature et type d’opération' => $nto,
-            //     'Nature des fonds' => $nb,
-            //     'Indicateur de présence RIB/BAN' => $pi,
-            //     'RIB' => $rib,
-            //     'Préfix IBAN' => $ip,
-            //     'Non, prénom ou raison sociale du donneur d’ordre' => $name,
-            //     'Adresse du donneur d’ordre' => $address,
-            //     'Date de la remise de l’ordre' => $date,
-            //     'Référence de la remise' => $discount_reference,
-            //     'Nombre d’opération dans la remise' => $discount_on,
-            //     'Montant total' => $totalAmount,
-            //     'Filler' => $filler,
-            // ];
+            $discount_header = utf8_encode(trim(mb_substr($line, 0, 4, 'UTF-8'))); // Entête de la remise
+            $iob = utf8_encode(trim(mb_substr($line, 4, 3, 'UTF-8'))); // Identifiant de la banque du donneur d’ordre
+            $nto = utf8_encode(trim(mb_substr($line, 7, 3, 'UTF-8'))); // Nature et type d’opération
+            $nb = utf8_encode(trim(mb_substr($line, 10, 1, 'UTF-8'))); // Nature des fonds
+            $pi = utf8_encode(trim(mb_substr($line, 11, 1, 'UTF-8'))); // Indicateur de présence RIB/BAN
+            $rib = utf8_encode(trim(mb_substr($line, 12, 20, 'UTF-8'))); // RIB
+            $ip = utf8_encode(trim(mb_substr($line, 32, 4, 'UTF-8'))); // Préfix IBAN
+            $name = utf8_encode(trim(mb_substr($line, 36, 50, 'UTF-8'))); // Nom, prénom ou raison sociale du donneur d’ordre
+            $address = utf8_encode(trim(mb_substr($line, 86, 70, 'UTF-8'))); // Adresse du donneur d’ordre
+            $date = utf8_encode(trim(mb_substr($line, 156, 8, 'UTF-8'))); // Date de la remise de l’ordre
+            $discount_reference = utf8_encode(trim(mb_substr($line, 164, 3, 'UTF-8'))); // Référence de la remise
+            $discount_on = utf8_encode(trim(mb_substr($line, 167, 6, 'UTF-8'))); // Nombre d’opération dans la remise
+            $totalAmount = utf8_encode(trim(mb_substr($line, 173, 16, 'UTF-8'))); // Montant total
+            $filler = utf8_encode(trim(mb_substr($line, 189, 31, 'UTF-8'))); // Filler
+
             return compact('discount_header', 'iob', 'nto', 'nb', 'pi', 'rib', 'ip', 'name', 'address', 'date', 'discount_reference', 'discount_on', 'totalAmount', 'filler', 'payment_id');
         }
         return [];
@@ -105,22 +89,12 @@ class File
             $ontto2 = utf8_encode(trim(mb_substr($line, 10, 1, 'UTF-8'))); // Numéro d'ordre de l'opération de virement
             $rib = utf8_encode(trim(mb_substr($line, 11, 20, 'UTF-8'))); // RIB
             $ip = utf8_encode(trim(mb_substr($line, 31, 4, 'UTF-8'))); // Préfix IBAN
-            $name = utf8_encode(trim(mb_substr($line, 35, 50, 'UTF-8'))); // Non, prénom ou raison sociale du client  bénéficiaire
+            $name = utf8_encode(trim(mb_substr($line, 35, 50, 'UTF-8'))); // Nom, prénom ou raison sociale du client bénéficiaire
             $address = utf8_encode(trim(mb_substr($line, 85, 70, 'UTF-8'))); // Adresse du client  bénéficiaire
             $amount = utf8_encode(trim(mb_substr($line, 155, 15, 'UTF-8'))); // Montant de l’opération de virement
             $label = utf8_encode(trim(mb_substr($line, 170, 70, 'UTF-8'))); // Libellé
             $filler = utf8_encode(trim(mb_substr($line, 240, 80, 'UTF-8'))); // Filler
-            // $data = [
-            //     "Numéro d'ordre de l'opération de virement" => $ontto,
-            //     "Numéro d'ordre de l'opération de virement 1" => $ontto2,
-            //     "RIB" => $rib,
-            //     "Préfix IBAN" => $ip,
-            //     "Non, prénom ou raison sociale du client  bénéficiaire" => $name,
-            //     "Adresse du client  bénéficiaire" => $address,
-            //     "Montant de l’opération de virement" => $amount,
-            //     "Libellé" => $label,
-            //     "Filler" => $filler
-            // ];
+
             return compact('ontto', 'ontto2', 'rib', 'ip', 'name', 'address', 'amount', 'label', 'filler', 'payment_id');
         }
         return [];
