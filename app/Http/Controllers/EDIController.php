fad065bbaf7e7f5e1fab8afc6a62061d52ae3afb
Ali Noual <noualdev@gmail.com>
2022-07-19 18:04:44 +0100

    Ajout de la mthode show, destroy et print

--- app/Http/Controllers/EDIController.php
+++ app/Http/Controllers/EDIController.php
@@ -3,21 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\EDI\StoreEdiFileRequest;
+use App\Models\Payment;
 use App\Services\File;
+use Illuminate\Http\Request;
 
 class EDIController extends Controller
 {
     public function index()
     {
-        $file = File::read('z7ncZvw0oqTvTchTvRRQcS5iibq6aeehbZVrw0uS.txt');
-        dd($file);
-        return view('pages.list');
+        $payments = Payment::with('issuer')->paginate();
+        return view('pages.list', compact('payments'));
+    }
+
+    public function show(Payment $payment)
+    {
+        $payment->load(['receivers', 'issuer']);
+        return view('pages.single', compact('payment'));
+    }
+
+    public function print(Payment $payment)
+    {
+        dd('impression en cours');
     }
 
     public function upload(StoreEdiFileRequest $storeEdiFileRequest)
     {
         $file = (new File)->upload($storeEdiFileRequest->file('edi_file'));
-
+        File::read($file->check());
         if ($file->check()) {
             return back()->withSuccess(['messages' => [
                 "Le fichier edi a été importer avec succés"
@@ -27,6 +39,21 @@ class EDIController extends Controller
         return back()->with('error', [
             'messages' => [
                 "Une erreur s'est produite lors de la tentative d'importation du fichier edi"
+            ]
+        ]);
+    }
+
+    public function destroy(Request $request, Payment $payment)
+    {
+        if ($payment->delete()) {
+            return redirect(route('edi.index'))->withSuccess(['messages' => [
+                "L'enregistrement n° {$payment->id} a été supprimer avec succés"
+            ]]);
+        }
+
+        return back()->with('error', [
+            'messages' => [
+                "Une erreur s'est produite lors de la tentative de suppression de l'enregistrement n° {$payment->id}"
             ]
         ]);
     }
