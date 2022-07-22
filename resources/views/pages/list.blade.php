0a29e460a9d64d328c40499cc22acff243c3603a
Ali Noual <noualdev@gmail.com>
2022-07-22 16:51:43 +0100

    Ajout d'informations à la vue

--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.default')
 @section('content')
-    @include('includes.search-bar')
+    <div class="columns">
+        <div class="column">
+            <h1 class="title is-2">Liste des remises</h1>
+            <h2 class="subtitle">Total: {{ $payments->count() }}</h2>
+        </div>
+        <div class="column">
+            @include('includes.search-bar')
+        </div>
+    </div>
     @if ($payments->count())
         @include('includes.payments')
     @else
